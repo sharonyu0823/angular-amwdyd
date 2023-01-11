94261ef6bef8d8e475865f6ad5835af37aced459
sharonyu0823 <111546956+sharonyu0823@users.noreply.github.com>
2023-01-11 17:51:07 +0800

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { Product } from '../products';
+import {Product} from '../products'
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css'],
 })
-export class ProductAlertsComponent {}
+export class ProductAlertsComponent {
+  @Input() product!: Product;
+}
